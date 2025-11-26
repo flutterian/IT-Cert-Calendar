34f59ac0a1c9d0cbe5733fb5bcb47940c2e3b4db
BYUNGMIN CHOI <bmchoi803@gmail.com>
2025-11-27 06:52:56 +0900

    eas 빌드를 위해 필요한 내역들 수정

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -21,12 +21,13 @@ if (__DEV__ && missingKeys.length) {
 }
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBpV7NGaIljWEcqBW8Znyre2bTuZ6tbf-E",
+  authDomain: "it-cert-calendar.firebaseapp.com",
+  projectId: "it-cert-calendar",
+  storageBucket: "it-cert-calendar.firebasestorage.app",
+  messagingSenderId: "813541178168",
+  appId: "1:813541178168:web:34711c4eb8a02e32621fcd",
+  measurementId: "G-JDLLWBX639",
 };
 
 console.log({ firebaseConfig });
